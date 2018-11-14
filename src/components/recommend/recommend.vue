df81a11725c471790ef809aebf988fe96b5f6f2b
yaoxiaodan <yaoxiaodan@dzqk.com>
2018-11-14 18:55:18 +0800

    jsonp的promise封装和数据抓取

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -5,8 +5,27 @@
 </template>
 
 <script>
+import {getRecommend} from 'api/recommend'
+import {ERR_OK} from 'api/config'
 export default {
-  name: 'recommend'
+  name: 'recommend',
+  data () {
+    return {
+      msg: '测试数据'
+    }
+  },
+  created () {
+    this._getRecommend()
+  },
+  methods: {
+    _getRecommend () {
+      getRecommend().then((res) => {
+        if (res.code === ERR_OK) {
+          console.log(res.data.slider)
+        }
+      })
+    }
+  }
 }
 </script>
 
