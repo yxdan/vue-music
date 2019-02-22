079591acb58491c94f4b28e11a70f65cff23fc82
yaoxiaodan <yaoxiaodan@dzqk.com>
2019-02-22 13:20:46 +0800

    推荐

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -116,6 +116,12 @@ export default {
           }
           .desc{
             .color_text_d;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            display: -webkit-box;
+            -webkit-line-clamp: 2;
+            -webkit-box-orient: vertical;
+            word-wrap: break-word;
           }
         }
       }
