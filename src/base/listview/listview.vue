f3a87bf07d006a05f3d382434d417fb6aee22075
yaoxiaodan <yaoxiaodan@dzqk.com>
2019-04-04 17:57:50 +0800

    歌手数据渲染

---
+++ src/base/listview/listview.vue
@@ -0,0 +1,115 @@
+<template>
+  <scroll class="listview" :data="data">
+    <ul>
+      <li class="list_group" v-for="(group, index) in data" :key="index">
+        <h2 class="list_group_title">{{group.title}}</h2>
+        <ul>
+          <li class="list_group_item" v-for="(item, index) in group.items" :key="index">
+            <img class="avatar" v-lazy="item.avatar" alt="">
+            <span class="name">{{item.name}}</span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </scroll>
+</template>
+
+<script>
+import Scroll from 'base/scroll/scroll'
+export default {
+  name: 'listview',
+  props: {
+    data: {
+      type: Array,
+      default: null
+    }
+  },
+  components: {
+    Scroll
+  }
+}
+</script>
+
+<style scoped lang="less">
+@import '../../common/less/variable.less';
+.listview{
+  position:relative;
+  width:100%;
+  height:100%;
+  overflow: hidden;
+  .color_bg;
+  .list_group{
+    padding-bottom:30px;
+    .list_group_title{
+      height:30px;
+      line-height:30px;
+      padding-left:20px;
+      .font_size_large;
+      .color_text_l;
+      .color_highlight_bg;
+    }
+    .list_group_item{
+      display:flex;
+      align-items: center;
+      padding: 10px 0 0 30px;
+      .avatar{
+        height:50px;
+        width:50px;
+        border-radius:50%;
+      }
+      .name{
+        margin-left:20px;
+        .color_text_l;
+        .font_size_medium;
+      }
+    }
+  }
+  .list_shortcut{
+    position:absolute;
+    right:0;
+    top:50%;
+    -webkit-transform: translateY(-50%);
+    -moz-transform: translateY(-50%);
+    -ms-transform: translateY(-50%);
+    -o-transform: translateY(-50%);
+    transform: translateY(-50%);
+    width:20px;
+    padding:20px 0;
+    -webkit-border-radius: 10px;
+    -moz-border-radius: 10px;
+    border-radius: 10px;
+    text-align:center;
+    .color_bg_d;
+    font-family: Helvetica;
+    .item{
+      padding:3px;
+      line-height:1;
+      .color_text_l;
+      .font_size_small;
+      &.current{
+        .color_theme;
+      }
+    }
+  }
+  .list_fixed{
+    position:absolute;
+    top:0;
+    left:0;
+    width:100%;
+    .fixed_title{
+      height:30px;
+      line-height:30px;
+      padding-left:20px;
+      .font_size_small;
+      .color_text_l;
+      .color_highlight_bg;
+    }
+  }
+  .loading_container{
+    position:absolute;
+    width:100%;
+    top:50%;
+    transform: translateY(-50%);
+  }
+}
+</style>
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    歌手页面
+    <list-view :data="singers" ref="list"></list-view>
   </div>
 </template>
 
 <script>
 import {getSingerList} from 'api/singer'
+import Singer from 'common/js/singer'
+import listView from 'base/listview/listview'
 import {ERR_OK} from 'api/config'
+const HOT_NAME = '热门推荐'
+const HOT_SINGER_LEN = 10
 export default {
   name: 'singer',
   data () {
@@ -17,18 +21,66 @@ export default {
   created () {
     this._getSingerList()
   },
+  components: {
+    listView
+  },
   methods: {
     _getSingerList () {
       getSingerList().then((res) => {
         if (res.code === ERR_OK) {
-          this.singers = res.data.list
+          this.singers = this._normalizeSinger(res.data.list)
         }
       })
+    },
+    _normalizeSinger (list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(new Singer({
+            name: item.Fsinger_name,
+            id: item.Fsinger_mid
+          }))
+        }
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(new Singer({
+          name: item.Fsinger_name,
+          id: item.Fsinger_mid
+        }))
+      })
+      // 为了得到有序列表
+      let hot = []
+      let ret = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      console.log(hot)
+      return hot.concat(ret)
     }
   }
 }
 </script>
 
 <style scoped>
-
+input{
+  border: 1px solid #e7e7e7;
+}
 </style>
